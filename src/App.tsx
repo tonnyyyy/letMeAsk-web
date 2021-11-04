145119ee20e43c0c45b893b9bd04f49c397d2a17
ti4 Gimi <tonyadryanogomes1@gmail.com>
2021-11-04 15:53:06 -0300

    configure firebase | change to typeScript

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+function App() {
+  return (
+    <div>aaaaaaaa</div>
+  );
+}
+
+export default App;
---
+++ src/services/firebase.ts
@@ -0,0 +1,20 @@
+import firebase from 'firebase/app';
+
+import 'firebase/auth';
+import 'firebase/database';
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSASING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export const auth = firebase;
+export const database = firebase;
